110bba7f19cab6155201d736bb429121d74c3f3d
Abi Vickery <vickeryabi@gmail.com>
2019-10-22 13:25:48 -0500

    jokes
    got yo mama jokes all written out

---
+++ javascript/app.js
@@ -0,0 +1,19 @@
+var ym1 = [
+    "yo mama so fat, Thanos had to snap twice!",
+    "yo mama so skinny, she uses her fingers as chop sticks!",
+    "yo mama so tiny, she hanglides on a dorito!",
+    "yo mama so stupid, she climbed over a glass wall just to see what was on the other side!",
+    "yo mama so fat, when she sat on the back of the bus it did a wheelie.",
+    "yo mama so stupid, she put airbags on her computer in case it crashed.",
+    "yo mama so fat, she needs cheat codes for Wii Fit.",
+    "Yo mama's so classless, she's a Marxist utopia.",
+    "yo mama so fat, that when she fell, no one was laughing but the ground was cracking up.",
+    "yo mama so stupid, she put lipstick on her forehead to make up her mind.",
+    "yo mama so fat,  I took a picture of her last Christmas and it's still printing.",
+    "yo mama so skinny, she hula hoops with a cherrio!",
+    "yo mama so tiny, she couldn't climb mountain dew!",
+    "yo mama so stupid,"
+]
+
+console.log(jokes[Math.floor(Math.random() * Math.floor(jokes.length))]);
+document.write(jokes[Math.floor(Math.random() * Math.floor(jokes.length))]);
