96f099d8c2e75e808e4f07b35717ec7d30c2cd20
Abi Vickery <vickeryabi@gmail.com>
2019-10-22 13:32:16 -0500

    spelling and css
    fixed spelling errors and added a css style sheet

--- javascript/app.js
+++ javascript/app.js
@@ -1,18 +1,17 @@
 var jokes = [
-    "yo mama so fat, Thanos had to snap twice!",
-    "yo mama so skinny, she uses her fingers as chop sticks!",
-    "yo mama so tiny, she hanglides on a dorito!",
-    "yo mama so stupid, she climbed over a glass wall just to see what was on the other side!",
-    "yo mama so fat, when she sat on the back of the bus it did a wheelie.",
-    "yo mama so stupid, she put airbags on her computer in case it crashed.",
-    "yo mama so fat, she needs cheat codes for Wii Fit.",
-    "Yo mama's so classless, she's a Marxist utopia.",
-    "yo mama so fat, that when she fell, no one was laughing but the ground was cracking up.",
-    "yo mama so stupid, she put lipstick on her forehead to make up her mind.",
-    "yo mama so fat,  I took a picture of her last Christmas and it's still printing.",
-    "yo mama so skinny, she hula hoops with a cherrio!",
-    "yo mama so tiny, she couldn't climb mountain dew!",
-    "yo mama so stupid,"
+    "Yo mama so FAT, Thanos had to snap twice!",
+    "Yo mama so SKINNY, she uses her fingers as chop sticks!",
+    "Yo mama so TINY, she hanglides on a dorito!",
+    "Yo mama so STUPID, she climbed over a glass wall just to see what was on the other side!",
+    "Yo mama so FAT, when she sat on the back of the bus it did a wheelie.",
+    "Yo mama so STUPID, she put airbags on her computer in case it crashed.",
+    "Yo mama so FAT, she needs cheat codes for Wii Fit.",
+    "Yo mama's so CLASSLESS, she's a Marxist utopia.",
+    "Yo mama so FAT, that when she fell, no one was laughing but the ground was cracking up.",
+    "Yo mama so STUPID, she put lipstick on her forehead to make up her mind.",
+    "Yo mama so FAT, I took a picture of her last Christmas and it's still printing.",
+    "Yo mama so SKINNY, she hula hoops with a cherrio!",
+    "Yo mama so TINY, she couldn't climb mountain dew!"
 ]
 
 console.log(jokes[Math.floor(Math.random() * Math.floor(jokes.length))]);
